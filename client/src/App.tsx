7e62ec80182659b0b899b93711720f0fc6240ad9
SickanDev <44079953+SickanDev@users.noreply.github.com>
2021-02-10 23:06:10 +0100

    Small fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,32 @@
+import dataUriToBuffer from 'data-uri-to-buffer';
 import React, {SyntheticEvent, useState} from 'react';
 import './style.css';
 
 function App() {
   const [imageSource, setImageSource] = useState<FileReader | null>(null);
 
-  const handleUpload = (e: SyntheticEvent<HTMLInputElement>) => {
+  const handleUpload = async (e: SyntheticEvent<HTMLInputElement>) => {
     let reader = new FileReader();
-    reader.readAsDataURL(((e?.target as HTMLInputElement).files as FileList)[0]);
+
+    await new Promise((resolve, reject) => {
+      reader.readAsDataURL(((e?.target as HTMLInputElement).files as FileList)[0]);
+      reader.onload = () => resolve(true);
+    });
+
+    if(typeof reader.result === "string"){
+      let buffer = dataUriToBuffer(reader.result);
+    }
+
     setImageSource(reader);
   }
 
-  console.log(imageSource)
 
   return (
     <div className="App">
       <input type="file" onChange={handleUpload}/>
       { typeof imageSource?.result === "string" ? 
           <img src={imageSource.result} />
-        : null 
+          : null
       }
     </div>
   );
